3fd984984e395bd8b27ced9141aff6c5756bf617
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-10 19:57:47 -0400

    Trying to get help on the manager join

--- db/schema.sql
+++ db/schema.sql
@@ -24,5 +24,7 @@ CREATE TABLE employees (
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
 );
