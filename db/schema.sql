c60bf8e170b77f29314010458b5161c46f07ff07
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-10 15:09:17 -0400

    Finalized the schema and the seed files

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS cms_db;
+CREATE DATABASE cms_db;
+
+USE cms_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dep_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary INT,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+);
