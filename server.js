def6c2e3ab762ef5f2200138a60b7284048deb0d
Sa Uk Chung <chungsauk95@gmail.com>
2022-07-16 00:18:34 -0400

    used the dotenv variables instead of hardcoded values, added an EXAMPLE.env

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const { prompt } = require("inquirer");
+require('dotenv').config()
 
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user: 'root',
-    // #### TYPE IN YOUR SQL PASSWORD HERE #####
-    password: '',
-    database: 'cms_db'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
   },
   console.log(`Connected to the cms_db database.`)
 );
